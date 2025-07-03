1bc60612262836b4262407f6234676b9619dfc33
Jeevakrishna <jeevakrishna073@gmail.com>
2025-07-03 06:55:32 +0530

    Created Window

---
+++ DX3D/Include/DX3D/All.h
@@ -0,0 +1,4 @@
+#pragma once
+#pragma once
+
+#include <DX3D/Game/Game.h>
---
+++ Game/main.cpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <DX3D/All.h>
+
+int main()
+{
+	dx3d::Game game{};
+	game.run();
+
+
+
+
+	return 0;
+}
